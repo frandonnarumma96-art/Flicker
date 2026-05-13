f3e22e208db603ada4f74c59f8a6bc170be73484
4mn3s1498 <ele24980@gmail.com>
2026-05-13 21:57:39 +0200

    startnavbar

---
+++ flicker/src/components/InterfacciaUtente/navbar.jsx
@@ -0,0 +1,17 @@
+export function Navbar () {
+    return (
+        <div>
+            <nav>
+<ul>
+    <li>Film</li>
+    <li>Serie Tv</li>
+    <li class="cuore">
+        <img src=""></img>
+    </li tag a e poi tag li con l immagine >
+    <li>Da vedere/Lista</li>
+    <li>Account</li>
+</ul>
+            </nav>
+        </div>
+    )
+}
