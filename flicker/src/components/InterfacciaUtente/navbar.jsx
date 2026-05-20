42d304bba444362e377c5913bfa09f135ba5fbf7
4mn3s1498 <ele24980@gmail.com>
2026-05-20 12:22:35 +0200

    updtNavbar

--- flicker/src/components/InterfacciaUtente/navbar.jsx
+++ flicker/src/components/InterfacciaUtente/navbar.jsx
@@ -1,38 +1,38 @@
+import { Clapperboard, Tv, Heart, Bookmark, User, BookMarked } from "lucide-react"
+
 export function Navbar() {
-
     return (
-        <div>
+        <nav className="fixed bottom-0 w-full bg-[#0d0d1f] border-t border-[#2a2a4a] py-2">
+            <ul className="flex justify-around items-center list-none m-0 p-0">
+                
+                <li className="flex flex-col items-center cursor-pointer gap-1">
+                    <Clapperboard size={24} color="#00ffff" />
+                    <span className="text-[#00ffff] text-xs">FILM</span>
+                </li>
 
-            <nav>
+                <li className="flex flex-col items-center cursor-pointer gap-1">
+                    <Tv size={24} color="gray" />
+                    <span className="text-gray-500 text-xs">SERIE TV</span>
+                </li>
 
-                <ul>
+                <li className="flex flex-col items-center cursor-pointer gap-1">
+                    <div className="bg-[#3a0060] rounded-xl p-2">
+                        <Heart size={24} color="#cc44ff" />
+                    </div>
+                    <span className="text-[#cc44ff] text-xs">MATCH</span>
+                </li>
 
-                    <li>
-                        <a href="">Film</a>
-                    </li>
+                <li className="flex flex-col items-center cursor-pointer gap-1">
+                    <BookMarked size={24} color="gray" />
+                    <span className="text-gray-500 text-xs">LISTA</span>
+                </li>
 
-                    <li>
-                        <a href="">Serie Tv</a>
-                    </li>
+                <li className="flex flex-col items-center cursor-pointer gap-1">
+                    <User size={24} color="gray" />
+                    <span className="text-gray-500 text-xs">ACCOUNT</span>
+                </li>
 
-                    <li className="cuore">
-                        <a href="">
-                            <img src="" alt="cuore" />
-                        </a>
-                    </li>
-
-                    <li>
-                        <a href="">Da vedere/Lista</a>
-                    </li>
-
-                    <li>
-                        <a href="">Account</a>
-                    </li>
-
-                </ul>
-
-            </nav>
-
-        </div>
+            </ul>
+        </nav>
     )
 }
