fec56674820836842758c6fdb3fa47d7af3820c0
Francesco Donnarumma <fran.donnarumma96@gmail.com>
2026-05-20 18:50:34 +0200

    fix

--- flicker/src/App.jsx
+++ flicker/src/App.jsx
@@ -1,12 +1,12 @@
 import Button from './components/InterfacciaUtente/button'
 import { Navbar } from './components/InterfacciaUtente/navbar'
 import Card from './components/InterfacciaUtente/card'
-
+import  './index.css'
 function App() {
   return (<div>
-    <Button text="Test" />
+    {/* <Button text="Test" /> */}
     <Navbar></Navbar>
-    <Card></Card>
+    {/* <Card></Card> */}
   </div>
 
   )
